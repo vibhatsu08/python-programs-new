ec866cde83494c1d977c639738ed5a91b8469612
Vedant Mistry <mistryvedant17@gmail.com>
2022-03-23 13:10:57 +0530

    python program to print the odd numbers in a given list.

---
+++ oddNumbersList.py
@@ -0,0 +1,6 @@
+# python program to print the odd numbers in a given list.
+list = [1, 23, 34, 23, 34, 45, 63, 65, 76, 3, 5, 65, 76, 45, 45, 374]
+
+for i in list :
+    if i%2 != 0 :
+        print(i)
